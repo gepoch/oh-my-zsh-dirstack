999219583bbacd2589013a815f364b4e8e9abf81
George Marchin <george.marchin@propylon.com>
2015-04-29 14:38:39 -0500

    Added ZSH_THEME_DIRSTACK_PROMPT_DELIMITER to allow for custom delimiters.

--- dirstack.plugin.zsh
+++ dirstack.plugin.zsh
@@ -2,8 +2,11 @@ alias p='pushd > /dev/null'
 alias o='popd > /dev/null'
 alias d='dirs -v'
 
+ZSH_THEME_DIRSTACK_PROMPT_DELIMITER=" "
+
 function dirstack_prompt_info {
-    extra_dirs=$(dirs -v | cut -d'	' -f2- | tail -n +2 |  tr "\\n" " ")
+    extra_dirs=$(echo -n "$(dirs -p | tail -n +2)")
+    extra_dirs=${extra_dirs//$'\n'/$ZSH_THEME_DIRSTACK_PROMPT_DELIMITER}
     if [ "$extra_dirs" ]; then
         echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_DIRSTACK_PROMPT_PREFIX$ZSH_THEME_DIRSTACK_NAME_COLOR$extra_dirs$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_DIRSTACK_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR"
     fi
